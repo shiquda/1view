11c61d4688200772279a80528117d1aa3b16dcaf
shiquda <czy1847259360@gmail.com>
2025-05-08 01:00:48 +0800

    fix(CI): fix path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import * as process from 'process';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react(), tailwindcss()],
 });
